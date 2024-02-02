87ac8cb0c19763ed3cfe4eb8d35a9ec428bc7cca
Francisco Ferreira <ferreiradeveloper@gmail.com>
2024-02-02 17:04:10 +0100

    instalacion y configuracion de passport,  modificaciones en bootstrap/app.php, config/auth.php, AuthServiceProvider.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -67,6 +67,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('auth');
 
 /*
 |--------------------------------------------------------------------------
@@ -98,8 +99,10 @@ $app->configure('app');
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
+//$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
